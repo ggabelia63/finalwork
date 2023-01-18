ce38734b3420485ee72b378fad0cc3d5b912872f
ggabelia <giorgi.gabelia.1@btu.edu.ge>
2023-01-18 20:42:18 +0400

    v0.1

--- app/src/main/java/com/example/mapsfinal/MainActivity.kt
+++ app/src/main/java/com/example/mapsfinal/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.mapsfinal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+
+        val mainFragment = mainFragment()
+        val fm: FragmentManager = supportFragmentManager
+        fm.beginTransaction().add(R.id.main_activity,mainFragment).commit()
+    }
 }
--- app/src/main/java/com/example/mapsfinal/mainFragment.kt
+++ app/src/main/java/com/example/mapsfinal/mainFragment.kt
@@ -18,41 +18,24 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class mainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-
-
-        }
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        val nextButton = view.findViewById<Button>(R.id.continueToMaps)
+        nextButton.setOnClickListener {
+            // Code to be executed when button is clicked
+            // For example, switch to another fragment
+            val locationFragment = locationFragment()
+            val fragmentManager = requireActivity().supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.main_activity,locationFragment)
+            fragmentTransaction.commit()
+        }
+
+        return view
     }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            mainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-
-
-
-
 }
